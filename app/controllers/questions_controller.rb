ebeb1303feed5f2fff680f1da898fb847120ce4a
jackymeslin <jackymeslin@hotmail.com>
2020-03-30 13:08:09 +0200

    params in progress...

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,18 @@
 class QuestionsController < ApplicationController
 
   def ask
+    # If the message is I am going to work, the coach will answer Great!
+    if params[:message] = "I am going to work"
+      p "Great !"
+      p "- Your coach"
+    # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+    # Otherwise the coach will answer I don't care, get dressed and go to work!
+    else "I don(t care, get dressed and go to work !"
+    end
+
+  end
+
+  def answer
   end
 
 end
